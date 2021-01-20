a55af049a338dd66e73d5a07484672077a6f0faa
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2021-01-20 13:41:48 -0300

    Implementing th Game Logic

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ console.log(document.querySelector('.guess').value);
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.number').textContent = secretNumber;
+let score = 20
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
@@ -24,10 +25,25 @@ document.querySelector('.check').addEventListener('click', function() {
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = '🎉 Correct Number!'
     // When the number's too high
-    } else if (guess < secretNumber) {
-        document.querySelector('.message').textContent = '📉 Too low'
-    // When the number's too low
     } else if (guess > secretNumber) {
-        document.querySelector('.message').textContent = '📈 Too high'
+        if(score > 1){
+            document.querySelector('.message').textContent = '📈 Too high'
+            score--;
+            document.querySelector('.score').textContent = score;
+        }else {
+            document.querySelector('.message').textContent = '💥 You lost the game.'
+            document.querySelector('.score').textContent = 0;
+        }
+    // When the number's too low
+    } else if (guess < secretNumber) {
+        if(score > 1){
+            document.querySelector('.message').textContent = '📈 Too low'
+            score--;
+            document.querySelector('.score').textContent = score;
+        }else {
+            document.querySelector('.message').textContent = '💥 You lost the game.'
+            document.querySelector('.score').textContent = 0;
+        }
+
     }
 });
