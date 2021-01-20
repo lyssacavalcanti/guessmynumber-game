31198671ac81af75e867955abae5edb8e41ce64b
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2021-01-20 07:11:24 -0300

    Implementing the Game Logic

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict'
 /*
 console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎉 Correct Number!'
+
 
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
@@ -10,12 +10,24 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
 
+    // When there's no input
     if(!guess) {
         document.querySelector('.message').textContent = ' ⛔️ No number!';
+    // When player wins
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = '🎉 Correct Number!'
+    // When the number's too high
+    } else if (guess < secretNumber) {
+        document.querySelector('.message').textContent = '📉 Too low'
+    // When the number's too low
+    } else if (guess > secretNumber) {
+        document.querySelector('.message').textContent = '📈 Too high'
     }
 });
-
